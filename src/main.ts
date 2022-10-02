7c01634c5092d2352b6fa512941750ad03023b98
Yurii Tereshchenko <yurater007@gmail.com>
2022-10-03 01:49:21 +0200

    feat: added API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Todo list app")
+    .setDescription("Todo list app API description")
+    .setVersion("1.0")
+    .addTag("todo")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
